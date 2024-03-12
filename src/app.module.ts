c40e068bbfd412a127a2bfa2d38efdf044582291
Suvigy Mishra <suvigymishra95@gmail.com>
2024-03-12 21:41:25 +0530

    Added changes for testing

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { GoApiWrapperModule } from "./modules/go-api-wrapper/go-api-wrapper.module";
 
+
+
 @Module({
   imports: [GoApiWrapperModule],
   providers: [AppService],
